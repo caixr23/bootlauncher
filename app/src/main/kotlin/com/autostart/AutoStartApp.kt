a12f69667667207374e9849782b255b7b72116c8
caixiangrong <caixiangrong@uniontech.com>
2026-05-18 09:50:39 +0800

    feat: wire up database in AutoStartApp

--- app/src/main/kotlin/com/autostart/AutoStartApp.kt
+++ app/src/main/kotlin/com/autostart/AutoStartApp.kt
@@ -1,5 +1,8 @@
 package com.autostart
 
 import android.app.Application
+import com.autostart.data.local.AppDatabase
 
-class AutoStartApp : Application()
+class AutoStartApp : Application() {
+    val database by lazy { AppDatabase.getDatabase(this) }
+}
